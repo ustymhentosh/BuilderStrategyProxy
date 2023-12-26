c13b23fcf15325df79c0e0e02dce2ac9091e1201
Ustym Hentosh <hentosh.pn@ucu.edu.ua>
2023-12-26 16:39:31 +0200

    new-fixes

--- src/main/java/com/example/task2/GiftMail.java
+++ src/main/java/com/example/task2/GiftMail.java
@@ -3,7 +3,7 @@ package com.example.task2;
 public class GiftMail implements MailStrategy {
     @Override
     public void sendMail(MailInfo mailInfo) {
-        System.out.println("Sending gift letter to " +
-         mailInfo.getClient().getName());
+        System.out.println("Sending gift letter to "
+                + mailInfo.getClient().getName());
     }
 }
